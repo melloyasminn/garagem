33dbf4ea4c5160561650d446517ed5988ba51f31
melloyasminn <melloyasminm@gmail.com>
2025-04-22 14:38:59 -0300

    Feat: Criação da API "Modelo"

--- app/urls.py
+++ app/urls.py
@@ -6,14 +6,14 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
-from core.views import UserViewSet, AcessorioViewSet, CorViewSet
+from core.views import AcessorioViewSet, CorViewSet, ModeloViewSet, UserViewSet
 
 router = DefaultRouter()
 
-router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
-router.register(r'cor', CorViewSet, basename='cor')
-
+router.register(r'cores', CorViewSet, basename='cores')
+router.register(r'modelos', ModeloViewSet, basename='modelos')
+router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .user import UserSerializer
 from .acessorio import AcessorioSerializer
 from .cor import CorSerializer
+from .modelo import ModeloSerializer
 
